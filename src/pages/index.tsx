702bec9a8c0b43b50c01d0ea8039b9d69c4c5994
andreitrinidad <andreitrinidad.dev@gmail.com>
2022-07-18 20:48:09 +0800

    🍱  Add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -391,6 +391,10 @@ const Home: NextPage = () => {
 			/>
 			<Head>
 				<title>ID Picture Print Layout Generator Tool</title>
+				<link
+					rel="icon"
+					href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🌄</text></svg>"
+				/>
 			</Head>
 			<AppHeader setTheme={setTheme} />
 
