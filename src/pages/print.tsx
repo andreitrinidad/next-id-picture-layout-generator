505bfb8850d39fb44351f8da30da92c48afcf6ef
andreitrinidad <andreitrinidad.dev@gmail.com>
2022-09-07 23:04:52 +0800

    ✨ Print Presets

--- src/pages/print.tsx
+++ src/pages/print.tsx
@@ -6,39 +6,44 @@ import { useEffect, useRef, useState } from 'react';
 import { useReactToPrint } from 'react-to-print';
 import useImageDimensions from '../hooks/useImageDimensions';
 import { useRouter } from 'next/router';
+import classNames from 'classnames';
 
 const PrintPreview: NextPage = () => {
 	const { data } = useImageContext();
 	const printRef = useRef<HTMLDivElement>(null);
-  const router = useRouter();
-	const { height, width } = useImageDimensions();
+	const router = useRouter();
+	const { height: _height, width: _width } = useImageDimensions();
 	const [paperSize, setPaperSize] = useState({
 		height: 7,
 		width: 5,
 		margin: 0.12,
+		isLandscape: false,
 	});
 	const ppi = 220;
 
+  let height = paperSize.isLandscape ? _width: _height;
+  let width = paperSize.isLandscape ? _height: _width;
+
 	const handlePrint = useReactToPrint({
 		content: () => printRef.current,
-    pageStyle: `
+		pageStyle: `
     @page {
       size: ${paperSize.width}in ${paperSize.height}in;
       margin: 0;
     }
-    `
+    `,
 	});
 
-  let image = '';
-  if (typeof window !== 'undefined') {
-     image = localStorage.getItem('image') || '';
-  }
+	let image = '';
+	if (typeof window !== 'undefined') {
+		image = localStorage.getItem('image') || '';
+	}
 
 	useEffect(() => {
-    if (image === '') {
-      router.back();
-    }
-    function detectKeys(event: any) {
+		if (image === '') {
+			router.back();
+		}
+		function detectKeys(event: any) {
 			if ((event.ctrlKey || event.metaKey) && event.keyCode == 80) {
 				event.preventDefault();
 				handlePrint();
@@ -83,7 +88,28 @@ const PrintPreview: NextPage = () => {
 			</header>
 			<main className="flex h-full border border-2">
 				{/* preview */}
-				<div className="flex flex-1 justify-center p-8 bg-base-200 h-full">
+				<div className="flex flex-1 flex-col justify-start items-center p-8 bg-base-200 h-full">
+        <div className="tooltip" data-tip="Orientation">
+					{/* <button className={
+            classNames("btn btn-square", 
+              !paperSize.isLandscape && 'btn-outline'
+            )
+          } onClick={() => {
+            	setPaperSize((prevState) => {
+                const newData = { ...prevState };
+                newData.isLandscape = !prevState.isLandscape;
+                return newData;
+              });
+          }}>
+						<Icon.RotateCw/>
+					</button> */}
+          </div>
+					<span className="badge badge-lg badge-neutral mb-4">
+						Paper Size: {paperSize.width} x {paperSize.height}
+					</span>
+					{/* <span className="badge badge-lg badge-neutral mb-4">
+						Image Size: {width} x {height}
+					</span> */}
 					<div
 						ref={printRef}
 						className=" bg-white overflow-hidden shadow-[0px_10px_60px_-15px_rgba(0,0,0,0.3)] print:bg-white print:shadow-none"
@@ -92,21 +118,35 @@ const PrintPreview: NextPage = () => {
 							width: `${paperSize.width}in`,
 						}}
 					>
-            <div style={{
-              			height: `${height}in`,
-								width: `${width}in`,
-            }}>
+						<div
+            // className="border-8 border-red-600 "
 
-						<img
 							style={{
-								// height: `${height}in`,
-								// width: `${width}in`,
-								margin: `${paperSize.margin}in`,
+								height: `${height}in`,
+								width: `${width}in`,
+                margin: `${paperSize.margin}in`,
 							}}
-							src={image}
-						/>
-            </div>
-
+						>
+							<img
+              className={classNames(
+                paperSize.isLandscape &&
+                  'rotate-90',
+                  //  'border-8 border-red-500 object-contain'
+              )}
+              // style={{
+							// 	height: `${height}in`,
+							// 	width: `${width}in`,
+              //   margin: `${paperSize.margin}in`,
+							// }}
+								// className='object-cover'
+								// className={classNames(
+                //   paperSize.isLandscape &&
+                //     'rotate-180',
+                //      'border-8 border-red-500'
+                // )}
+								src={image}
+							/>
+						</div>
 					</div>
 				</div>
 
@@ -123,8 +163,8 @@ const PrintPreview: NextPage = () => {
 							Inches
 						</span>
 					</div>
-          <div className="divider m-0"></div>
-          <div className="flex gap-2 items-center justify-between mb-2">
+					<div className="divider m-0"></div>
+					<div className="flex gap-2 items-center justify-between mb-2">
 						<label className="flex-1 uppercase font-bold text-sm">
 							width
 						</label>
@@ -136,16 +176,16 @@ const PrintPreview: NextPage = () => {
 							value={paperSize.width}
 							onChange={(e) => {
 								let value = Number(e.target.value);
-								setPaperSize((prevState => {
-                  const newData = {...prevState}
-                  newData.width = value;
-                  return newData
-                }));
+								setPaperSize((prevState) => {
+									const newData = { ...prevState };
+									newData.width = value;
+									return newData;
+								});
 							}}
 						/>
-            in
+						in
 					</div>
-          <div className="flex gap-2 items-center justify-between mb-2">
+					<div className="flex gap-2 items-center justify-between mb-2">
 						<label className="flex-1 uppercase font-bold text-sm">
 							Height
 						</label>
@@ -157,16 +197,16 @@ const PrintPreview: NextPage = () => {
 							value={paperSize.height}
 							onChange={(e) => {
 								let value = Number(e.target.value);
-								setPaperSize((prevState => {
-                  const newData = {...prevState}
-                  newData.height = value;
-                  return newData
-                }));
+								setPaperSize((prevState) => {
+									const newData = { ...prevState };
+									newData.height = value;
+									return newData;
+								});
 							}}
 						/>
-            in
+						in
 					</div>
-          <div className="flex gap-2 items-center justify-between mb-2">
+					<div className="flex gap-2 items-center justify-between mb-2">
 						<label className="flex-1 uppercase font-bold text-sm">
 							margin
 						</label>
@@ -178,21 +218,70 @@ const PrintPreview: NextPage = () => {
 							value={paperSize.margin}
 							onChange={(e) => {
 								let value = Number(e.target.value);
-								setPaperSize((prevState => {
-                  const newData = {...prevState}
-                  newData.margin = value;
-                  return newData
-                }));
+								setPaperSize((prevState) => {
+									const newData = { ...prevState };
+									newData.margin = value;
+									return newData;
+								});
 							}}
 						/>
-            in
+						in
 					</div>
 
-          <div className="flex gap-2 justify-between mt-8">
+					<div className="flex gap-2 justify-between mt-8">
 						<h3 className="font-bold">Presets</h3>
 					</div>
-          <div className="divider m-0"></div>
-         
+					<div className="divider m-0"></div>
+					<div className="flex gap-2 flex-wrap">
+						<button
+							className="btn btn-neutral btn-outline gap-2"
+							onClick={() => {
+								setPaperSize((prevState) => {
+									const newData = { ...prevState };
+									newData.width = 5;
+									newData.height = 7;
+									return newData;
+								});
+							}}
+						>
+							5x7
+							<span className="badge badge-neutral !text-neutral badge-outline">
+								5R
+							</span>
+						</button>
+						<button
+							className="btn btn-neutral btn-outline gap-2"
+							onClick={() => {
+								setPaperSize((prevState) => {
+									const newData = { ...prevState };
+									newData.width = 4;
+									newData.height = 6;
+									return newData;
+								});
+							}}
+						>
+							4x6
+							<span className="badge badge-neutral !text-neutral badge-outline">
+								4R
+							</span>
+						</button>
+						<button
+							className="btn btn-neutral btn-outline gap-2"
+							onClick={() => {
+								setPaperSize((prevState) => {
+									const newData = { ...prevState };
+									newData.width = 3.5;
+									newData.height = 5;
+									return newData;
+								});
+							}}
+						>
+							3.5x5
+							<span className="badge badge-neutral !text-neutral badge-outline">
+								3R
+							</span>
+						</button>
+					</div>
 				</div>
 			</main>
 		</div>
